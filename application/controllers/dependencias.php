1c146f8c985a17898329240892b7baf96078c491
SunnySup <sonia.yo.x@gmail.com>
2015-11-10 17:36:24 -0600

    Citla

--- application/controllers/dependencias.php
+++ application/controllers/dependencias.php
@@ -15,7 +15,7 @@ class Dependencias extends CI_Controller {
 	{
 		$this->load->library('form_validation');
 
-		$this->form_validation->set_rules('dependencia','Dependencia','required');
+		$this->form_validation->set_rules('dependencia','Dependencia','required|is_unique[dependencias.dependencia]');
 		$this->form_validation->set_rules('titular','Titular','required');
 		$this->form_validation->set_rules('calle','Calle','required');
 		$this->form_validation->set_rules('numExt','Numero Exterior','required');
@@ -24,9 +24,22 @@ class Dependencias extends CI_Controller {
 		$this->form_validation->set_rules('localidad','Localidad','required');
 		$this->form_validation->set_rules('tel1','Telefono 1','required');
 
+		$this->form_validation->set_message('is_unique',"Esa dependencia ya existe");
+
 		if($this->form_validation->run())
 		{
-			redirect('/dependencias/');	
+			
+			$this->load->model('model_dependencias');
+
+			if($this->model_dependencias->add_dependencia())
+			{
+				redirect('/dependencias/');	
+			}else{
+				echo "registro no exitoso";
+				
+				redirect('/dependencias/');
+			}
+
 		}else{
 			$this->load->view('template/header');
 		$this->load->view('LAY/dependencias');
--- application/models/model_dependencias.php
+++ application/models/model_dependencias.php
@@ -2,5 +2,31 @@
 
 class Model_dependencias extends CI_Model
 {
-	
+	public function add_dependencia()
+	{
+		$data = array(
+			'dependencia' => $this->input->post('dependencia'),			
+			'titular' => $this->input->post('titular'),
+			'calle' => $this->input->post('calle'),
+			'numExt' => $this->input->post('numExt'),
+			'numInt' => $this->input->post('numInt'),
+			'colonia' => $this->input->post('colonia'),
+			'localidad' => $this->input->post('localidad'),
+			'cp' => $this->input->post('cp'),
+			'tel1' => $this->input->post('tel1'),
+			'tel2' => $this->input->post('tel2')
+			
+			);
+
+		$q= $this->db->insert('dependencias',$data);
+
+		if($q)
+		{
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+
 }
