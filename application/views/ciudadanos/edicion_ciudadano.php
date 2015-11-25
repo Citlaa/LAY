3eb9d7db8f612cc72f7e66d3263f34f95c1e349b
DianaSaucedo <dlsl_00@hotmail.com>
2015-11-25 09:34:38 -0600

    Dayan

--- application/views/ciudadanos/edicion_ciudadano.php
+++ application/views/ciudadanos/edicion_ciudadano.php
@@ -1,12 +1,55 @@
 <section>
-  <h2>Edición de ciudadano</h2>
+<head>
+    <link href="<?php echo base_url("assets/css/bootstrap.min.css");?>" rel="stylesheet">
+  
+
+</head>               
+<body>       
+
+                              
+    <!-- Quick setting box starts -->
+      <div class="quick-slide hidden-xs"></div>         
+      <div class="out-container">
+         <div class="outer">
+            <!-- Sidebar starts -->
+            
+            <!-- Sidebar ends -->
+            
+            <!-- Mainbar starts -->
+            <div class="mainbar">
+               
+          <!-- Page heading starts -->
+        <div class="page-head">
+        <!-- Page heading ends -->
+        <!-- Black block ends -->
+        
+        <!-- Content starts -->
+        
+        <div class="container">
+          <div class="page-content">
+            <!-- Content starts -->
+        
+        <div class="container">
+        <div class="page-content page-users">
+            <div class="page-tabs">
+
+              <!-- Tab panes -->
+              <div class="tab-content">
+                <div class="tab-pane fade active in" id="search">
+          
+                  <div class="widget">
+                  
+                    
+              
+                    <div class="clearfix"></div>    
+                    <h2>Edición de ciudadano</h2>
   <div class="container-fluid">
 
     <?php echo validation_errors(); ?>
     <?php echo form_open('ciudadanos/editar_ciudadano', array('class'=>'form-horizontal')); ?>
     <input type="hidden" name="idCiudadano" value="<?php echo $ciudadano[0]['idCiudadano'];?>">
     <div class="form-group">
-      <label for="nombre" class="col-sm-2 control-label">Nombre</label>
+      <label for="nombre" class="col-sm-2 control-label">Nombre:</label>
       <div class="col-sm-10">
         <input type="text" name="nombre" class="form-control" value="<?php echo $ciudadano[0]['nombre']; ?>">
       </div>
@@ -39,5 +82,28 @@
     <a  href="<?php echo site_url('regidenu/mostrar_busqueda');?>" class="btn btn-danger">Cancelar</a>
   </form>
 </div>
+            
+                </div>                                     
+                </div>
+                
+<!-- Add new Registro -->
 
+<!--hasta aqui -->
+
+              </div>
+            </div>
+          </div>
+      </div>
+          </div>
+        </div>
+        <!-- Content ends -->       
+        </div>
+            <!-- Mainbar ends -->
+            
+            <div class="clearfix"></div>
+         </div>
+      </div>  
+
+     
+  </body>      
 </section>
