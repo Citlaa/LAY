649d57b418c978128e5196262c2a73d11929abc6
DianaSaucedo <dlsl_00@hotmail.com>
2015-11-30 02:32:19 -0600

    Dayan

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:82/lay/';
+
+$config['base_url'] = 'http://localhost:8080/lay/';
+
+
 
 /*
 |--------------------------------------------------------------------------
