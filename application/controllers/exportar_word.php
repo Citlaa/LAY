caa38fd055e5135dbb0b7da23ec1cf17e86d5423
José Cardona Figueroa <cardona5d@gmail.com>
2015-12-01 12:24:22 -0600

    Se agregó prueba de marca de agua de lay

--- application/controllers/exportar_word.php
+++ application/controllers/exportar_word.php
@@ -15,24 +15,28 @@ class Exportar_Word extends CI_Controller{
   {
     $this->load->library('word');
     //our docx will have 'lanscape' paper orientation
-    $section = $this->word->createSection(array('orientation'=>'landscape'));
-    // Add text elements
-    $section->addText('Hello World!');
-    $section->addTextBreak(1);
-    $filename='just_some_random_name.docx'; //save our document as this file name
-header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document'); //mime type
-header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
-header('Cache-Control: max-age=0'); //no cache
+    $section = $this->word->createSection();
+    // Create header
+    $header = $section->createHeader();
 
-$objWriter = PHPWord_IOFactory::createWriter($this->word, 'Word2007');
-$objWriter->save('php://output');
+    // Add a watermark to the header
+    $header->addWatermark('plantilla.png', array('marginTop'=>-35, 'marginLeft'=>-85));
+
+    $filename = "descarga.docx";
+    header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document'); //mime type
+    header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
+    header('Cache-Control: max-age=0'); //no cache
+
+    $objWriter = PHPWord_IOFactory::createWriter($this->word, 'Word2007');
+    $objWriter->save('php://output');
   }
 
   public function template()
   {
     $this->load->library('word');
     $document = new PHPWord_Template(APPPATH.'assets\word_template\prueba.docx');
-    // $document = $this->word->loadTemplate('application/assets/word_template/prueba.docx');
+    // $document = $this->word->loadTemplate(APPPATH.'assets\word_template\prueba.docx');
+    var_dump($document);exit;
     $document->setValue('ciudadano', 'José Cardona Figueroa');
      $filename='just_some_random_name.docx'; //save our document as this file name
     header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document'); //mime type
