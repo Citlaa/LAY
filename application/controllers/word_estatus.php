73609333360ac1f73c7331e512a807892ce45088
DianaSaucedo <dlsl_00@hotmail.com>
2015-12-02 21:40:33 -0600

    WORD estatus terminado

--- application/controllers/word_estatus.php
+++ application/controllers/word_estatus.php
@@ -8,60 +8,65 @@ class Word_Estatus extends CI_Controller{
     parent::__construct();
 
   }
-
   function index($idEstatus)
   {
     $this->load->library('word');
     //our docx will have 'lanscape' paper orientation
-    $section = $this->word->createSection();
-    // Create header
+    $section = $this->word->createSection(array('orientation' => 'landscape'));
+   
     $header = $section->createHeader();
 
     // Add a watermark to the header
     $header->addWatermark('plantilla.png', array('marginTop'=>-35, 'marginLeft'=>-85));
 
+    $styleTable = array('borderSize'=>6, 'borderColor'=>'000000', 'cellMargin'=>80);
+    $styleFirstRow = array('valign'=>'center', 'marginTop'=>90, 'borderBottomSize'=>14, 'borderBottomColor'=>'FFFFF', 'bgColor'=>'393939');
+    
+    $this->load->model(array('model_denuncias'));
 
-    $styleTable = array('borderSize'=>6, 'borderColor'=>'006699', 'cellMargin'=>80);
-    $styleFirstRow = array('borderBottomSize'=>18, 'borderBottomColor'=>'0000FF', 'bgColor'=>'66BBFF');
-
-    // Define cell style arrays
-    $styleCell = array('valign'=>'center');
+    $styleCell = array('jc'=>'center','valign'=>'center');
     $styleCellBTLR = array('valign'=>'center', 'textDirection'=>PHPWord_Style_Cell::TEXT_DIR_BTLR);
 
     // Define font style for first row
     $fontStyle = array('bold'=>true, 'align'=>'center');
 
+    // Query
+    $resultados = $this->model_denuncias->by_estatus($idEstatus);
+    $estatus = "Reporte de ". $resultados[0]['estatus']; 
+
     // Add table style
-    $this->word->addTableStyle('myOwnTableStyle', $styleTable, $styleFirstRow);
+    $this->word->addTableStyle('myOwnTableStyle', $styleTable, $styleFirstRow , $sectionStyle);
+    $section->addText(" ",array("size"=>12,"bold"=>true));
+    $section->addText("Reporte Generado",array("color"=>"3B0B17", "size"=>14,"bold"=>true));
+    $section->addText($estatus, array("size"=>10,"bold"=>true));
 
     // Add table
-    $table = $section->addTable('myOwnTableStyle');
+    $table = $section->addTable('myOwnTableStyle', $sectionStyle);
 
     // Add row
-    $table->addRow(900);
+    $table->addRow(90);
 
     $table->addCell(2000, $styleCell)->addText('Fecha', $fontStyle);
     $table->addCell(2000, $styleCell)->addText('Ciudadano', $fontStyle);
     $table->addCell(2000, $styleCell)->addText('Dependencia', $fontStyle);
-    $table->addCell(2000, $styleCell)->addText('Recepcion', $fontStyle);
+    $table->addCell(2000, $styleCell)->addText('Estatus', $fontStyle);
+    $table->addCell(2000, $styleCell)->addText(utf8_decode('Recepción'), $fontStyle);
     $table->addCell(500, $styleCell)->addText('Asunto', $fontStyle);
-    $table->addCell(500, $styleCell)->addText('Direccion', $fontStyle);
+    $table->addCell(500, $styleCell)->addText(utf8_decode('Dirección'), $fontStyle);
 
-    $this->load->model(array('model_denuncias'));
-
-    $resultados = $this->model_denuncias->by_estatus($idEstatus);
 
     foreach ($resultados as $resultado) {
       $table->addRow();
-      $table->addCell(2000)->addText("".$resultado['fecha']);
-      $table->addCell(2000)->addText("".$resultado['ciudadano']);
-    	$table->addCell(2000)->addText("".$resultado['dependencia']);
-    	$table->addCell(2000)->addText("".$resultado['recepcion']);
-      $table->addCell(2000)->addText("".$resultado['asunto']);
-      $table->addCell(2000)->addText("".$resultado['direccion']);
+      $table->addCell(2000)->addText("".utf8_decode($resultado['fecha']));
+      $table->addCell(2000)->addText("".utf8_decode($resultado['ciudadano']));
+      $table->addCell(2000)->addText("".utf8_decode($resultado['dependencia']));
+      $table->addCell(2000)->addText("".utf8_decode($resultado['estatus']));
+      $table->addCell(2000)->addText("".utf8_decode($resultado['recepcion']));
+      $table->addCell(2000)->addText("".utf8_decode($resultado['asunto']));
+      $table->addCell(2000)->addText("".utf8_decode($resultado['direccion']));
     }
 
-    $filename = "DenunciasPorCiudadano.docx";
+    $filename = "DenunciasPorEstatus.docx";
     header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document'); //mime type
     header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
     header('Cache-Control: max-age=0'); //no cache
